20c309daca48646ad14eabdd76a478f98788654b
Kim in sool <pdg300@naver.com>
2023-02-10 01:43:02 +0900

    #20230210 KIS - 001
    키사 ECB 암호화 모듈 추가

--- is-blog-back/src/main/java/com/web/blog/common/controller/CommonController.java
+++ is-blog-back/src/main/java/com/web/blog/common/controller/CommonController.java
@@ -1,13 +1,29 @@
 package com.web.blog.common.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.web.blog.common.component.CtyptKisaSeedEcbTemplate;
 
 @RestController
 public class CommonController {
 	
+	@Autowired
+	CtyptKisaSeedEcbTemplate ctypt;
+	
 	@GetMapping("/isblog/back/test")
 	public String selectTestMsg() {
+		
+		String test = "Hello, Spring Boot!";
+		
+		System.out.println("ctypt origin val : " + test);
+		
+		String ctyTest = ctypt.Encrypt_SEED(test);
+		
+		System.out.println("ctypt enc val : " + ctyTest);
+		
+		System.out.println("ctypt dec val : " + ctypt.Decrypt_SEED(ctyTest));
 		
 		return "Hello, Spring Boot!";
 	}
